bf579fc2a155cc24186190cc49fb7a7587ef6aa7
Matheus Lopes <matheuslopes.ufrrj@gmail.com>
2022-08-31 17:53:53 -0300

    game mania

--- js/gamemania.js
+++ js/gamemania.js
@@ -6,3 +6,29 @@ function mostrarMenu(){
         menu.style.display="none"
     }
 }
+
+$(document).ready(function () {
+    let bannerAtual=1
+    let listaBanners= ["banner-pc1", "banner-pc2", "banner-pc3"]
+    setInterval(mudarBanner, 4000);
+
+    function mudarBanner() {
+        if (bannerAtual>0) {
+            $("#carrosselIndex").removeClass(listaBanners[bannerAtual-1])
+            
+        }else{
+            $("#carrosselIndex").removeClass(listaBanners[2])
+    
+        }
+        
+         $("#carrosselIndex").addClass(listaBanners[bannerAtual])
+        
+         bannerAtual++
+        
+        if (bannerAtual>2) {
+            bannerAtual=0
+        }
+    }
+    
+    
+})
